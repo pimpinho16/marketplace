a8aba5dad308bbfcf10af05610865275098499d5
Alexander Andrade <alexander.amedina@gmail.com>
2019-11-30 14:21:40 -0600

    agregando desarrollo

---
+++ src/components/CreateProducto.vue
@@ -0,0 +1,82 @@
+<template>
+<div id="app">
+    <vue-form-generator :schema="schema"></vue-form-generator>
+</div>
+</template>
+<script>
+import VueFormGenerator from "vue-form-generator"
+export default{
+    name:"app",
+    components: {
+            "vue-form-generator": VueFormGenerator.component
+        },
+    data(){
+        return {
+           /* model:{
+                precioregular: 0,
+                preciofinal: precioCalculado()
+            },*/
+            schema:{
+                fields:[
+                    {
+                    type: "input",
+                    inputType: "text",
+                    label: "Empresa",
+                    model: "empresa",
+                    placeholder: "Empresa",
+                    featured: true,
+                    required: true
+                    },
+                    {
+                    type: "input",
+                    inputType: "text",
+                    label: "Producto",
+                    model: "producto",
+                    placeholder: "Nombre de producto",
+                    featured: true,
+                    required: true
+                    },
+                    {
+                    type: "input",
+                    inputType: "text",
+                    label: "Descripcion",
+                    model: "descripcion",
+                    placeholder: "Descripcion",
+                    featured: true,
+                    required: true
+                    },
+                    {
+                    type: "input",
+                    inputType: "text",
+                    label: "Precio Regular",
+                    model: "precioregular",
+                    placeholder: "Precio Regular",
+                    featured: true,
+                    required: true
+                    },
+                    {
+                    type: "input",
+                    inputType: "text",
+                    label: "Descuento",
+                    model: "descuento",
+                    placeholder: "Descuento",
+                    featured: true,
+                    required: true
+                    },
+                     {
+                    type: "input",
+                    inputType: "text",
+                    label: "Precio final",
+                    model: "preciofinal",
+                    placeholder: "Precio Final",
+                    featured: false,
+                    required: true,
+                    enable: false
+                    },
+                ]
+            }
+           
+         }        
+}
+}
+</script>
---
+++ src/components/ListProducto.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="app">
+    <h3>Listado de productos</h3>
+   <table style="center">
+     <thead>
+     <th v-bind:key="key" v-for="key in columns">{{key}}</th>
+     </thead>
+     <tbody >
+      <tr v-bind:key="prod" v-for="prod in products"  >
+        <td>{{prod.name}}</td>
+        <td>{{prod.precioregular}}</td>
+        <td>{{prod.descuento}}</td>
+        <td>{{prod.iniciovigencia}}</td>
+        <td>{{prod.finvigencia}}</td>
+        <td>{{prod.empresa}}</td>
+      </tr>
+     </tbody>
+   </table>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+  data(){
+    return{
+      products:[
+        {name:'Nintendo 64',precioregular:100.0,descuento:10,iniciovigencia:"10-Nov-2019",finvigencia:"31-Dec-2019",empresa:"Radio Shark" }
+
+      ],
+      columns:[
+        "Producto",
+        "Precio Regular",
+        "Descuento",
+        "Inicio de vigencia descuento",
+        "Fin de vigencia descuento",
+        "Empresa"
+      ]
+    }
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+table {
+  border: 2px solid #42b983;
+  border-radius: 3px;
+  background-color: #fff;
+}
+
+th {
+  background-color: #42b983;
+  color: rgba(255,255,255,0.66);
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+td {
+  background-color: #f9f9f9;
+}
+</style>
